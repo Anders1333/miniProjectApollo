43be01cde564b2a41f57457e9e1c97d8dbed84ab
Anders <andershc92@hotmail.com>
2018-11-27 09:40:11 +0100

    last changes

--- app.js
+++ app.js
@@ -20,8 +20,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/api', apiRouter);
+app.use('/friendfinder', indexRouter);
+app.use('/friendfinder/api', apiRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
